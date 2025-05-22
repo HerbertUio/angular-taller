4aa2b3fa1c4045dd1d9cd9f2cafe904a2bf1fa6d
Herbert Daniel <herbert.bustamante@uab.edu.bo>
2025-05-22 10:19:30 -0400

    user-list component finalizado

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,18 +1,34 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, Pipe, PipeTransform } from '@angular/core';
 import { UserService } from '../service/service.component';
+import { CommonModule } from '@angular/common';
+
+
+
+@Pipe({ name: 'genderTransform', standalone: true })
+export class GenderTransformPipe implements PipeTransform {
+  transform(value: string): string {
+    return value === 'M' ? 'Masculino' : 'Femenino';
+  }
+}
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, GenderTransformPipe],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
 export class UserListComponent implements OnInit  {
-
-  private userService = inject(UserService);
   ngOnInit(): void {
-    this.userService.getAllUsers().subscribe(
-      users => {console.log(users);})
+    this.userService.getAllUsers().subscribe({
+      next: (users: any) => {
+        this.users = users;
+      },
+    }
+    );
   }
+
+  users: any[] = [];
+  userService = inject(UserService);
+
 }
